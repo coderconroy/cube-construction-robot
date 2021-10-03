8b2ebce14664e6e32a4f165fccf05935c3908063
u18072918 <u18072918@tuks.co.za>
2021-10-03 02:48:46 +0200

    Add live camera feed to home screen

--- software/HomeView.cpp
+++ software/HomeView.cpp
@@ -1,8 +1,44 @@
 #include "HomeView.h"
-#include "opencv2/opencv.hpp"
 #include "iostream"
 
 HomeView::HomeView(QWidget* parent) : QWidget(parent)
 {
+    // Initialize camera feed label
+    cameraFeed = new QLabel(this);
 
+    // Initialize layout
+    layout = new QVBoxLayout(this);
+
+    // Add camera label to layout
+    layout->addWidget(cameraFeed);
+
+    // Add layout to view
+    setLayout(layout);
+
+    // Initialize camera
+    camera = new cv::VideoCapture(0);
+    if (!camera->isOpened()) 
+        std::cerr << "ERROR: No camera found" << std::endl;
+
+    // Initialize camera feed timer
+    cameraFeedTimer = new QTimer(this);
+    connect(cameraFeedTimer, &QTimer::timeout, this, &HomeView::updateCameraFeed);
+    cameraFeedTimer->start(20); // Update feed every 20ms
+}
+
+HomeView::~HomeView()
+{
+    delete camera;
+}
+
+void HomeView::updateCameraFeed()
+{
+    // Capture frame from camera
+    cv::Mat frame;
+    *camera >> frame;
+
+    // Display image in camera feed
+    cvtColor(frame, frame, cv::COLOR_BGR2RGB); // COnvert from BGR to RGB
+    QImage cameraFeedImage = QImage((uchar*) frame.data, frame.cols, frame.rows, frame.step, QImage::Format_RGB888);
+    cameraFeed->setPixmap(QPixmap::fromImage(cameraFeedImage));
 }
--- software/HomeView.h
+++ software/HomeView.h
@@ -1,10 +1,41 @@
-#include <qwidget.h>
+#include <QWidget>
+#include <QLabel>
+#include <QVBoxLayout>
+#include <QTimer>
+#include "opencv2/opencv.hpp"
+
+/*!
+* The \class HomeView class defines the layout and operation of the home screen. 
+* The home screen is the initial screen presented to the user when the software is started.
+* The purpose of the home screen is to ensure that the required camera hardware as well as 
+* the robotic system are present and connected. Furthermore, it also acts as a branching point
+* to access the other components of the system.. This includes the shape specification, construction
+* and qualification testing components.
+*/
 
 class HomeView : public QWidget
 {
     Q_OBJECT
 
 public:
+    /*!
+    * Class constructor. Initializes the home screen.
+    * @param [in] parent Parent widget
+    */
     HomeView(QWidget* parent = Q_NULLPTR);
 
+    /*!
+    * Class destructor.
+    */
+    ~HomeView();
+private:
+    QLabel* cameraFeed; /*! Displays live images captured by camera*/
+    QVBoxLayout* layout; /*! Base layout for the home screen*/
+    QTimer* cameraFeedTimer; /*! Timebase to refresh camera feed display*/
+    cv::VideoCapture* camera; /*! Source of live camera images*/
+
+    /*!
+    * Captures new image from camera and updates the camera feed.
+    */
+    void updateCameraFeed(); 
 };
