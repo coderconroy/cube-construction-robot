64b6701a427518a26f7edd36b0c98c5d2a3f0bee
u18072918 <u18072918@tuks.co.za>
2021-10-10 22:02:05 +0200

    Complete Cube class implementation

--- software/Cube.cpp
+++ software/Cube.cpp
@@ -1,9 +1,11 @@
 #include "Cube.h"
 
-Cube::Cube(QObject* parent) : QObject(parent)
+Cube::Cube(unsigned int& sideLength, QObject* parent) : QObject(parent)
 {
+	// Initialize member variables
+	this->sideLength = sideLength;
 	position = glm::vec3(0, 0, 0);
-	pose = glm::vec3(0, 0, 0);
+	orientation = glm::vec3(0, 0, 0);
 }
 
 void Cube::setPosition(const glm::vec3& position)
@@ -11,9 +13,14 @@ void Cube::setPosition(const glm::vec3& position)
 	this->position = position;
 }
 
-void Cube::setPose(const glm::vec3& pose)
+void Cube::setOrientation(const glm::vec3& orientation)
 {
-	this->pose = pose;
+	this->orientation = orientation;
+}
+
+unsigned int Cube::getSideLength()
+{
+	return sideLength;
 }
 
 glm::vec3 Cube::getPosition() const
@@ -21,7 +28,7 @@ glm::vec3 Cube::getPosition() const
 	return position;
 }
 
-glm::vec3 Cube::getPose() const
+glm::vec3 Cube::getOrientation() const
 {
-	return pose;
+	return orientation;
 }
--- software/Cube.h
+++ software/Cube.h
@@ -3,21 +3,56 @@
 #include <QObject>
 #include <glm/glm.hpp>
 
+/*!
+* Represents a construction cube in terms of its size, 3D centroid position and orientation.
+* The position of the cube is represented using Cartesian coordinates with respect to the world frame.
+* The local cube frame assumes a right-hand coordinate system. The orientation of the cube is parameterized
+* using Euler angles where phi, theta and psi correspond to rotation about the x, y and z axes respectively.
+*/
 class Cube: public QObject
 {
 	Q_OBJECT
 public:
 	/*!
-	* Class constructor
+	* Class constructor.
+	* \param [in] sideLength Length of the cube edges in terms of steps.
+	* \param [in] parent Parent widget.
 	*/
-	Cube(QObject* parent);
+	Cube(unsigned int& sideLength, QObject* parent);
 
+	/*!
+	* Cube position setter.
+	* \param [in] position Cartesian position of the cube centre (x, y, z) in steps.
+	*/
 	void setPosition(const glm::vec3& position);
-	void setPose(const glm::vec3& pose);
+
+	/*!
+	* Cube orientation setter.
+	* \param [in] orientation Orientation of cube parameterized using Euler angles (phi, theta, psi) in radians.
+	*/
+	void setOrientation(const glm::vec3& orientation);
+
+	/*!
+	* Cube side length getter.
+	* \return Length of the cube edges in terms of steps.
+	*/
+	unsigned int getSideLength();
+
+	/*!
+	* Cube position getter.
+	* \return Cartesian position of the cube centre (x, y, z) in steps.
+	*/
 	glm::vec3 getPosition() const;
-	glm::vec3 getPose() const;
+
+	/*!
+	* Cube orientation setter.
+	* \return Orientation of cube parameterized using Euler angles (phi, theta, psi) in radians.
+	*/
+	glm::vec3 getOrientation() const;
+
 private:
-	glm::vec3 position; /*! Cartesian position of the cube centre (x, y, z) */
-	glm::vec3 pose; /*! Orientation of cube parameterized using Euler angles (phi, theta, psi) */
+	unsigned int sideLength; /*! Length of the cube edges in terms of steps */
+	glm::vec3 position; /*! Cartesian position of the cube centre (x, y, z) in steps */
+	glm::vec3 orientation; /*! Orientation of cube parameterized using Euler angles (phi, theta, psi) in radians */
 };
 
