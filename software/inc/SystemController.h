c54a22b32a0aa093f996300e2a714a0f117ba5bb
u18072918 <u18072918@tuks.co.za>
2021-10-14 16:41:45 +0200

    Create logger class source files

--- software/inc/SystemController.h
+++ software/inc/SystemController.h
@@ -21,12 +21,15 @@ private:
     QHBoxLayout* navigationLayout; /*! Layout for software navigation components */
     QVBoxLayout* baseLayout; /* Base layout for the program views and navigation components */
     QStackedLayout* viewLayout; /* Layout container for the current primary view */
+
     HomeView* homeView; /*! Initial view to establish hardware connections */
     DesignView* designView; /*! 3D shape definition view */
     ConstructionView* constructionView; /*! View to initiate and monitor the construction of the 3D shape */
     QPushButton* homeViewLink; /*! Navigation link for home view */
     QPushButton* designViewLink; /*! Navigation link for design view */
     QPushButton* constructionViewLink; /*! Navigation link for construction view */
+
+    QTextEdit* messageLog; /*! Display for all messages logged by various software components */
 
     /*!
     * Set the current primary view based on which link button was clicked.
---
+++ software/src/Logger.cpp
@@ -0,0 +1,6 @@
+#include "Logger.h"
+
+Logger::Logger(QWidget* parent): QWidget(parent)
+{
+
+}
