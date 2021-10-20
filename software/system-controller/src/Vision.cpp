1bebc6188c09fd1b9f8b46c2b4c408e2c0b35473
u18072918 <u18072918@tuks.co.za>
2021-10-20 21:18:13 +0200

    Implement world point to image point projection

--- software/system-controller/src/Vision.cpp
+++ software/system-controller/src/Vision.cpp
@@ -1,18 +1,19 @@
 #include "Vision.h"
+#include <iostream>
 
 Vision::Vision(QObject* parent) : QObject(parent)
 {
 	// Initialize world points
 	std::vector<cv::Point3d> worldPoints;
-	worldPoints.push_back(cv::Point3d(545, 830, -64));
-	worldPoints.push_back(cv::Point3d(543, 519, -64));
-	worldPoints.push_back(cv::Point3d(540, 206, -64));
-	worldPoints.push_back(cv::Point3d(889, 827, -64));
-	worldPoints.push_back(cv::Point3d(888, 518, -64));
-	worldPoints.push_back(cv::Point3d(886, 207, -64));
-	worldPoints.push_back(cv::Point3d(1233, 826, -64));
-	worldPoints.push_back(cv::Point3d(1232, 515, -64));
-	worldPoints.push_back(cv::Point3d(1232, 204, -64));
+	worldPoints.push_back(cv::Point3d(0, 0, -64));
+	worldPoints.push_back(cv::Point3d(507, 0, -64));
+	worldPoints.push_back(cv::Point3d(1015, 0, -64));
+	worldPoints.push_back(cv::Point3d(0, 562, -64));
+	worldPoints.push_back(cv::Point3d(507, 562, -64));
+	worldPoints.push_back(cv::Point3d(1015, 562, -64));
+	worldPoints.push_back(cv::Point3d(0, 1125, -64));
+	worldPoints.push_back(cv::Point3d(507, 1125, -64));
+	worldPoints.push_back(cv::Point3d(1015, 1125, -64));
 
 	// Initialize image points
 	std::vector<cv::Point2d> imagePoints;
@@ -27,8 +28,8 @@ Vision::Vision(QObject* parent) : QObject(parent)
 	imagePoints.push_back(cv::Point2d(1232, 204));
 
 	// Initialize camera matrix and distortion coefficients
-	cv::Mat cameraMatrix = (cv::Mat_<double>(3, 3) << fx, 0, cx, 0, fy, cy, 0, 0, 1);
-	cv::Mat distCoeffs = cv::Mat::zeros(4, 1, cv::DataType<double>::type);
+	cameraMatrix = (cv::Mat_<double>(3, 3) << fx, 0, cx, 0, fy, cy, 0, 0, 1);
+	distCoeffs = cv::Mat::zeros(4, 1, cv::DataType<double>::type);
 
 	// Compute pose of world frame with respect to the fixed camera frame
 	cv::Mat rotationVector;
@@ -40,10 +41,16 @@ Vision::Vision(QObject* parent) : QObject(parent)
 
 cv::Point3d Vision::projectImagePoint(const cv::Point2d& imagePoint, double z) const
 {
+
 	return cv::Point3d();
 }
 
 cv::Point2d Vision::projectWorldPoint(cv::Point3d worldPoint) const
 {
-	return cv::Point2d();
+	// Compute homogenous image point
+	cv::Mat imagePoint = cameraMatrix * (rotationMatrix * cv::Mat(worldPoint, false) + translationVector);
+
+	// Normalize and return uv frame image point
+	double s = imagePoint.at<double>(2);
+	return cv::Point2d(imagePoint.at<double>(0) / s, imagePoint.at<double>(1) / s);
 }
