34e62cecaae7b6ffd6e635efb0f9bd3d8f047bad
u18072918 <u18072918@tuks.co.za>
2021-10-20 20:24:03 +0200

    Implement Vision class initialisation

--- software/system-controller/inc/Vision.h
+++ software/system-controller/inc/Vision.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <QObject>
+#include "opencv2/opencv.hpp"
 
 class Vision : public QObject
 {
@@ -10,8 +11,31 @@ public:
 	* Class constructor.
 	* @param [in] parent Parent object.
 	*/
-	Vision(QObject* parent);
+	Vision(QObject* parent = Q_NULLPTR);
+
+	/*!
+	* Compute the corresponding world point given an image point and the Z coordinate of the world point.
+	* \param [in] imagePoint Coordinates of the source point in the uv image frame.
+	* \param [in] z Z coordinate of the correspoding world point.
+	* \return World point with specified z coordinate corresponding to given image point.
+	*/
+	cv::Point3d projectImagePoint(const cv::Point2d& imagePoint, double z) const;
+
+	/*!
+	* Compute the corresponding image point given a world point.
+	* \param [in] worldPoint Coordinates of the source point in the XYZ world frame.
+	* \return Image point corresponding to given world point.
+	*/
+	cv::Point2d projectWorldPoint(cv::Point3d worldPoint) const;
+
 private:
+	double fx = 696.2920653066839 * 2; /*! Camera x-axis focal length */
+	double fy = 696.1538823160478 * 2; /*! Camera y-axis focal length */
+	double cx = 469.7644569362635 * 2; /*! Camera principal point x-coordinate */
+	double cy = 281.0969237061734 * 2; /*! Camera principal point y-coordinate */
 
+	cv::Mat cameraMatrix; /*! Intrinsic camera matrix */
+	cv::Mat distCoeffs; /*! Camera distorition coefficients */
+	cv::Mat rotationMatrix; /*! Rotation matrix for world frame with respect to camera frame */
+	cv::Mat translationVector; /*! Translation matrix for world frame with respect to camera frame */
 };
-
--- software/system-controller/src/Vision.cpp
+++ software/system-controller/src/Vision.cpp
@@ -1,6 +1,49 @@
 #include "Vision.h"
 
-Vision(QObject* parent) : QObject(parent)
+Vision::Vision(QObject* parent) : QObject(parent)
 {
+	// Initialize world points
+	std::vector<cv::Point3d> worldPoints;
+	worldPoints.push_back(cv::Point3d(545, 830, -64));
+	worldPoints.push_back(cv::Point3d(543, 519, -64));
+	worldPoints.push_back(cv::Point3d(540, 206, -64));
+	worldPoints.push_back(cv::Point3d(889, 827, -64));
+	worldPoints.push_back(cv::Point3d(888, 518, -64));
+	worldPoints.push_back(cv::Point3d(886, 207, -64));
+	worldPoints.push_back(cv::Point3d(1233, 826, -64));
+	worldPoints.push_back(cv::Point3d(1232, 515, -64));
+	worldPoints.push_back(cv::Point3d(1232, 204, -64));
 
+	// Initialize image points
+	std::vector<cv::Point2d> imagePoints;
+	imagePoints.push_back(cv::Point2d(545, 830));
+	imagePoints.push_back(cv::Point2d(543, 519));
+	imagePoints.push_back(cv::Point2d(540, 206));
+	imagePoints.push_back(cv::Point2d(889, 827));
+	imagePoints.push_back(cv::Point2d(888, 518));
+	imagePoints.push_back(cv::Point2d(886, 207));
+	imagePoints.push_back(cv::Point2d(1233, 826));
+	imagePoints.push_back(cv::Point2d(1232, 515));
+	imagePoints.push_back(cv::Point2d(1232, 204));
+
+	// Initialize camera matrix and distortion coefficients
+	cv::Mat cameraMatrix = (cv::Mat_<double>(3, 3) << fx, 0, cx, 0, fy, cy, 0, 0, 1);
+	cv::Mat distCoeffs = cv::Mat::zeros(4, 1, cv::DataType<double>::type);
+
+	// Compute pose of world frame with respect to the fixed camera frame
+	cv::Mat rotationVector;
+	cv::solvePnP(worldPoints, imagePoints, cameraMatrix, distCoeffs, rotationVector, translationVector);
+
+	// Convert rotation vector to rotation matrix
+	cv::Rodrigues(rotationVector, rotationMatrix);
+}
+
+cv::Point3d Vision::projectImagePoint(const cv::Point2d& imagePoint, double z) const
+{
+	return cv::Point3d();
+}
+
+cv::Point2d Vision::projectWorldPoint(cv::Point3d worldPoint) const
+{
+	return cv::Point2d();
 }
